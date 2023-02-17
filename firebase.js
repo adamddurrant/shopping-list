3f5cd5f81056dad39a8565d3e283ec4a772f6a99
Adam Durrant <adamdurrant92@gmail.com>
2023-02-17 21:24:30 +0000

    prepped for Vercel hosting

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDoDhxQs3vsV50tf81nXfvqHmfMND2fr0w',
-  authDomain: 'shopping-list-9ec59.firebaseapp.com',
-  projectId: 'shopping-list-9ec59',
-  storageBucket: 'shopping-list-9ec59.appspot.com',
-  messagingSenderId: '515698754491',
-  appId: '1:515698754491:web:bb9d20722f631c43e23a9e',
-  measurementId: 'G-S1R4PX27JD',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "shopping-list-9ec59.firebaseapp.com",
+  projectId: "shopping-list-9ec59",
+  storageBucket: "shopping-list-9ec59.appspot.com",
+  messagingSenderId: "515698754491",
+  appId: process.env.FIREBASE_AP_ID_KEY,
+  measurementId: "G-S1R4PX27JD",
 };
 
 // Initialize Firebase
