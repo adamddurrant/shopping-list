cc47d545aa65bb4c025cd2acbe134fdc6c641ce7
Adam Durrant <adamdurrant92@gmail.com>
2023-02-17 22:16:18 +0000

    updated for vercel env details

--- firebase.js
+++ firebase.js
@@ -1,12 +1,14 @@
+import FIREBASE_API_KEY from "./config.js";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: FIREBASE_API_KEY,
   authDomain: "shopping-list-9ec59.firebaseapp.com",
   projectId: "shopping-list-9ec59",
   storageBucket: "shopping-list-9ec59.appspot.com",
   messagingSenderId: "515698754491",
-  appId: process.env.FIREBASE_AP_ID_KEY,
+  appId: "1:515698754491:web:bb9d20722f631c43e23a9e",
   measurementId: "G-S1R4PX27JD",
 };
 
