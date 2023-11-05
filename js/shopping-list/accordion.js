61007f2aff90d7fe2f84047044617839700538fc
Adam Durrant <adam@Adams-MacBook-Pro.local>
2023-11-05 11:40:19 +0000

    Add accordions

---
+++ js/shopping-list/accordion.js
@@ -0,0 +1,19 @@
+addEventListener("DOMContentLoaded", (event) => {
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      console.log('run');
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+  
+});
