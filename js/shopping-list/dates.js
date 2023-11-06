9d99c98685d3f7dc8ac45cd68d12fe77cb5a7b31
Adam Durrant <adam@Adams-MacBook-Pro.local>
2023-11-06 19:55:55 +0000

    Add date submit buttons and clear input button

--- js/shopping-list/dates.js
+++ js/shopping-list/dates.js
@@ -8,9 +8,16 @@
 function handleInput(inputValue) {
   // Perform your action with the input value
   const dateWrapper = document.getElementById('date-wrap');
+  const reset = document.getElementById('reset-meals');
+
   if (inputValue.trim() === "") {
     dateWrapper.classList.remove('active');
   } else {
     dateWrapper.classList.add('active');
   }
+
+  reset.addEventListener("click", function(){
+    dateWrapper.classList.remove('active');
+  });
+
 }
